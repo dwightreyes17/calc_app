f28e129b711a6fbc9ac2deae2a76191eaf478ad7
dwightreyes17 <111841503+dwightreyes17@users.noreply.github.com>
2022-10-26 09:31:43 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,80 +1,136 @@
-import { useState} from 'react';
+import { useState } from "react";
 import './App.css';
+
+function CalcButton({value, onClick}) {
+
+  return (
+    <button className = "calcButton" onClick ={onClick}>
+       {value}
+    </button>
+  );
+}
 
 function CalcDisplay({display}) {
   return (
-    <div className= " calcDisplay ">
-     {display}
+    <div className = "calcDisplay">
+      {display}
     </div>
   );
 }
 
-function CalcButton({value, onClick}) {
-  return (
-    <button className="calcButton" onClick={onClick}>
-        {value}
-    </button>
-  );
-}
-    
 function App() {
 
-  
-    const [num1, setNum1] = useState (0);
-    const [num2, setNum2] = useState (0);
-    const [oper, setOper] = useState (0);
-    const [res, setRes] = useState (0);
-    const [disp, setDisp] = useState (0);
+ 
+  const[num1, setNum1] = useState(0);
+  const[oper, setOper] = useState(0);
+  const[num2, setNum2] = useState(0);
+  const[res, setRes] = useState(0);
+  const[disp, setDisp] = useState(0);
 
-    const numberClickHandler = (e) => {
-      e.preventDefault();
-      const value = e.target.innerHTML;
 
-      if (oper === 0){
-        if (num1 === 0) {
-        setNum1(value)
-        setDisp(value);
-      } else {
-        setNum1(num1+value)
-        setDisp(num1+value);
+  const numberClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    var numLength = num1.toString().length
+ 
+
+    if (numLength < 16){
+      if(oper === 0){
+        setRes(0);
+
+        if (value === "0" && num1 === 0){
+          setNum1(0);
+          setDisp(0);
+        }else if (num1 === 0) {
+          setNum1(value)
+          setDisp(value);
+        } else{
+          setNum1(num1 + value)
+          setDisp(num1 + value);
+        }
+
+      } else if (num2 === 0) {
+          setNum2(value)
+          setDisp(value);
+          if (res !== 0){
+            setNum1(res);
+          }
+      } else{
+        setNum2(num2 + value)
+        setDisp(num2 + value);
       }
-    } else {
-      setNum2(num2+value)
-      setDisp(num2+value);      
+    
     }
-    console.log(num1 +'|'+ num2 +'|'+ oper +'|'+ res +'|'+ disp);
+    
+    console.log(num1 + '|' + num2 + '|' + oper + '|' + res + '|' + 'value:' + value + 'length'+numLength )
   };
+
 
   const operClickHandler = (e) => {
     e.preventDefault();
     const value = e.target.innerHTML;
-    setOper(value)
+    setOper(value);
     setDisp(value);
 
-    console.log(num1 +'|'+ num2 +'|'+ oper +'|'+ res +'|'+ disp);
+    console.log(num1 + '|' + num2 + '|' + oper + '|' + res + '|' + disp);
   };
 
   const equalClickHandler = () => {
-    console.log(num1 +'|'+ num2 +'|'+ oper +'|'+ res +'|'+ disp);
-    
+    console.log(num1 + '|' + num2 + '|' + oper + '|' + res + '|' + disp);
     if(oper === "+") {
-      setRes(parseInt(num1) + parseInt(num2));
-      setDisp(parseInt(num1) + parseInt(num2));
-    } else if (oper === "-") {
-      setRes(parseInt(num1) - parseInt(num2));
-      setDisp(parseInt(num1) - parseInt(num2));      
-    } else {
-      setDisp("ERROR");
+      setRes(parseFloat(num1) + parseFloat(num2))
+      setDisp(parseFloat(num1) + parseFloat(num2));
+      if (oper === "+"){
+            setNum1(0);
+            setNum2(0);
+            setOper(0);
+      }else if (oper === 0){
+            setRes(0);
+          }
+     
+    }else if (oper === "-"){
+      setRes(parseFloat(num1) - parseFloat(num2));
+      setDisp(parseFloat(num1) - parseFloat(num2));
+      if (oper === "-"){
+            setNum1(0);
+            setNum2(0);
+            setOper(0);
+      }else if (oper === 0){ 
+            setRes(0);
+          }
+     
+    }else if (oper === "*"){
+      setRes(parseFloat(num1) * parseFloat(num2));
+      setDisp(parseFloat(num1) * parseFloat(num2));
+      if (oper === "*"){
+            setNum1(0);
+            setNum2(0);
+            setOper(0);
+      }else if (oper === 0){
+            setRes(0);
+          }
+     
+    }else if (oper === "/"){
+      setRes(parseFloat(num1) / parseFloat(num2));
+      setDisp(parseFloat(num1) / parseFloat(num2));
+      if (oper === "/"){
+            setNum1(0);
+            setNum2(0);
+            setOper(0);
+      }else if (oper === 0){
+            setRes(0);
+          }
+     
+    }else {
+      setDisp("ERROR")
       setNum1(0);
       setNum2(0);
       setOper(0);
-      setRes(0);
-      setDisp(0);            
+      setDisp(0);
     }
   };
 
-  /*This is now working!*/
-  const clearClickHandler = () => {
+ const clearClickHandler = () => {
     setNum1(0);
     setNum2(0);
     setOper(0);
@@ -83,30 +139,75 @@ function App() {
 
     console.log(num1 +'|'+ num2 +'|'+ oper +'|'+ res +'|'+ disp);
   };
+  
+  const percentClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    if (oper === 0){
+      setNum1(parseFloat(num1) /100);
+      setDisp(num1 + value);
+      setRes(parseFloat(num1) /100);
+    }else{
+      setNum2(parseFloat(num2) /100);
+      setDisp(num2 + value);
+      setRes(parseFloat(num2) /100);
+    }
+    
 
-    return ( 
-    <div className="calcContainer">
-      <CalcDisplay display={disp}/>
-      <div className="calcButtonsContainer">
-        <CalcButton value="7" onClick={numberClickHandler}/>
-        <CalcButton value="8" onClick={numberClickHandler}/>
-        <CalcButton value="9" onClick={numberClickHandler}/>
-        <CalcButton value="+" onClick={operClickHandler}/>
-        <CalcButton value="4" onClick={operClickHandler}/>
-        <CalcButton value="5" onClick={operClickHandler}/>
-        <CalcButton value="6" onClick={operClickHandler}/>
-        <CalcButton value="-" onClick={operClickHandler}/>
-        <CalcButton value="3" onClick={operClickHandler}/>
-        <CalcButton value="2" onClick={operClickHandler}/>
-        <CalcButton value="1" onClick={operClickHandler}/>
-        <CalcButton value="*" onClick={operClickHandler}/>
-        <CalcButton value="Clear" onClick={clearClickHandler}/>
-        <CalcButton value="0" onClick={numberClickHandler}/>
-        <CalcButton value="=" onClick={equalClickHandler}/>
-        <CalcButton value="/" onClick={operClickHandler}/>
-      </div>  
-    </div>  
-  );
+    console.log(oper + value + res)
+  }
+
+  const signClickHandler = () => {
+    if (oper === 0){
+      setNum1(-num1);
+      setDisp(-num1);
+    }else{
+      setNum2(-num2);
+      setDisp(-num2);
+    }
+    
+  };
+
+  const decimalClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    if (oper === 0){
+    setNum1(num1 + value);
+    setDisp(num1 + value);
+    }else{
+      setNum2(num2 + value);
+      setDisp(num2 + value);
+    }
+ 
+  };
+  return(
+
+    <div className = "calcContainer">
+    <CalcDisplay display = {disp}/>
+
+    <div className = "calcButtonsContainer">
+       <CalcButton value = "7" onClick={numberClickHandler}/>
+       <CalcButton value = "8" onClick={numberClickHandler}/>
+       <CalcButton value = "9" onClick={numberClickHandler}/>
+       <CalcButton value = "+" onClick={operClickHandler}/>
+       <CalcButton value = "6" onClick={numberClickHandler}/>
+       <CalcButton value = "5" onClick={numberClickHandler}/>
+       <CalcButton value = "4" onClick={numberClickHandler}/>
+       <CalcButton value = "-" onClick={operClickHandler}/>
+       <CalcButton value = "3" onClick={numberClickHandler}/>
+       <CalcButton value = "2" onClick={numberClickHandler}/>
+       <CalcButton value = "1" onClick={numberClickHandler}/>
+       <CalcButton value = "*" onClick={operClickHandler}/>
+       <CalcButton value = "%"onClick={percentClickHandler}/>
+       <CalcButton value = "."onClick={decimalClickHandler}/>
+       <CalcButton value = "0" onClick={numberClickHandler}/>
+       <CalcButton value = "/"onClick={operClickHandler}/>
+       <CalcButton value = "C" onClick={clearClickHandler}/>
+       <CalcButton value = "+-"onClick={signClickHandler}/>
+       <CalcButton value = "=" onClick={equalClickHandler}/>
+    </div>
+
+    </div>
+  )
 }
-
 export default App;
